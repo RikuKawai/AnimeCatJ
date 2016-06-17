b531528c46832f1d0fe25fcf270654a1f01e5c2f
RikuKawai <riku@thisis.moe>
2016-06-17 10:53:22 -0400

    updated documentation and added connection test

--- src/moe/thisis/animecat/view/UIController.java
+++ src/moe/thisis/animecat/view/UIController.java
@@ -1,6 +1,7 @@
 package moe.thisis.animecat.view;
 
 import java.io.File;
+import java.net.URL;
 
 import javafx.fxml.FXML;
 
@@ -20,6 +21,12 @@ import moe.thisis.animecat.Main;
 
 import moe.thisis.animecat.model.Anime;
 
+/**
+ * UIController.java
+ * Controller class for main GUI
+ * @author	Quinlan McNellen
+ * @date	2016/06/17
+ */
 public class UIController {
 	@FXML
 	private TableView<Anime> animeTable;
@@ -54,6 +61,9 @@ public class UIController {
 	@FXML
 	private Button controlAbout;
 	
+	/**
+	 * Show about dialog when the about button is clicked
+	 */
 	@FXML
 	private void showAbout() {
 		Alert alert = new Alert(AlertType.INFORMATION);
@@ -63,26 +73,32 @@ public class UIController {
 		
 		alert.showAndWait();
 	}
-	// Event Listener on Button[#controlAdd].onAction
+	/**
+	 * Show edit dialog when the add button is clicked
+	 */
 	@FXML
 	public void entryAdd() {
-		Anime tempAnime = new Anime();
-		boolean okClicked = mainApp.showAnimeDialog(tempAnime);
+		Anime tempAnime = new Anime(); //temporary Anime entry
+		boolean okClicked = mainApp.showAnimeDialog(tempAnime); //open dialog for temporary Anime
 		if (okClicked) {
-			mainApp.getAnimeData().add(tempAnime);
+			mainApp.getAnimeData().add(tempAnime); //add temporary Anime to list
 		}
 	}
-	// Event Listener on Button[#controlSave].onAction
+	/**
+	 * Save list to file
+	 */
 	@FXML
 	public void listSave() {
-		File animeFile = mainApp.getAnimeFilePath();
+		File animeFile = mainApp.getAnimeFilePath(); //get path of current file
 		if (animeFile != null) {
-			mainApp.saveAnimeDataToFile(animeFile);
+			mainApp.saveAnimeDataToFile(animeFile); //save list
 		} else {
-			listSaveAs();
+			listSaveAs(); //call save as if there is no open file
 		}
 	}
-	// Event Listener on Button[#controlQuit].onAction
+	/**
+	 * Load list from file
+	 */
 	@FXML
 	public void listLoad() {
 		FileChooser fileChooser = new FileChooser();
@@ -91,17 +107,23 @@ public class UIController {
 				"XML files (*.xml)", ".xml");
 		fileChooser.getExtensionFilters().add(extFilter);
 		
-		File file = fileChooser.showOpenDialog(mainApp.getPrimaryStage());
+		File file = fileChooser.showOpenDialog(mainApp.getPrimaryStage()); //open file dialog
 		
 		if (file != null) {
-			mainApp.loadAnimeDataFromFile(file);
+			mainApp.loadAnimeDataFromFile(file); //load list
 		}
 	}
+	/**
+	 * Create new blank list
+	 */
 	@FXML
 	public void listNew() {
-		mainApp.getAnimeData().clear();
-		mainApp.setAnimeFilePath(null);
+		mainApp.getAnimeData().clear(); //clear data
+		mainApp.setAnimeFilePath(null); //clear file path
 	}
+	/**
+	 * Choose file name and save list
+	 */
 	@FXML
 	public void listSaveAs() {
 		FileChooser fileChooser = new FileChooser();
@@ -110,22 +132,24 @@ public class UIController {
 				"XML files (*.xml)", "*.xml");
 		fileChooser.getExtensionFilters().add(extFilter);
 		
-		File file = fileChooser.showSaveDialog(mainApp.getPrimaryStage());
+		File file = fileChooser.showSaveDialog(mainApp.getPrimaryStage()); //save file dialog
 		
 		if (file != null) {
-			if (!file.getPath().endsWith(".xml")) {
+			if (!file.getPath().endsWith(".xml")) { //append .xml to file if the user did not
 				file = new File(file.getPath());
 			}
-			mainApp.saveAnimeDataToFile(file);
+			mainApp.saveAnimeDataToFile(file); //save list
 		}
 	}
-	// Event Listener on Button[#controlRemove].onAction
+	/**
+	 * Remove entry from list
+	 */
 	@FXML
 	private void entryRemove() {
-		int selectedIndex = animeTable.getSelectionModel().getSelectedIndex();
-		if (selectedIndex >= 0) {
-			animeTable.getItems().remove(selectedIndex);
-		} else {
+		int selectedIndex = animeTable.getSelectionModel().getSelectedIndex(); //get selected entry
+		if (selectedIndex >= 0) { //check if anything is selected
+			animeTable.getItems().remove(selectedIndex); //remove selected entry
+		} else { //throw error if nothing is selected
 			Alert alert = new Alert(AlertType.WARNING);
 			alert.initOwner(mainApp.getPrimaryStage());
 			alert.setTitle("No Selection");
@@ -142,16 +166,37 @@ public class UIController {
 	public UIController() {
 		
 	}
-	
+	/**
+	 * Initialize table with two columns
+	 */
 	@FXML
 	private void initialize() {
 		animeTitleColumn.setCellValueFactory(cellData -> cellData.getValue().animeTitleProperty());
 		animeIDColumn.setCellValueFactory(cellData -> cellData.getValue().animeIDProperty());
 		
-		showAnimeDetails(null);
+		checkConnection(); //check Internet connection
 		
-		animeTable.getSelectionModel().selectedItemProperty().addListener(
+		showAnimeDetails(null); //show null details
+		
+		animeTable.getSelectionModel().selectedItemProperty().addListener( //listen for selections and show details
 				(observable, oldValue, newValue) -> showAnimeDetails(newValue));
+	}
+	/**
+	 * Checks Internet connection by trying to connect to Google
+	 */
+	private void checkConnection() {
+		try {
+			URL url = new URL("http://google.ca");
+			url.openStream(); //attempts to open connection
+		} catch (Exception e) { //catches connection failure
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("No Connection");
+			alert.setHeaderText("Connection Failed");
+			alert.setContentText("Please check your Internet connection");
+			
+			alert.showAndWait();
+			System.exit(0); //closes the application, as it cannot function properly offline
+		}
 	}
 	
 	public void setMain(Main mainApp) {
@@ -159,7 +204,10 @@ public class UIController {
 		
 		animeTable.setItems(mainApp.getAnimeData());
 	}
-	
+	/**
+	 * Update the details section with details for an Anime
+	 * @param	anime	Anime to show details for
+	 */
 	private void showAnimeDetails(Anime anime) {
 		if (anime != null) {
 			animeTitleLabel.setText(anime.getAnimeTitle());
@@ -167,14 +215,14 @@ public class UIController {
 			metaYearLabel.setText(anime.getMetaYear());
 			metaStatusLabel.setText(anime.getMetaStatus());
 			metaRatingLabel.setText(anime.getMetaRating());
-			metaCover.setImage(new Image(anime.getImageURL()));
+			metaCover.setImage(new Image(anime.getImageURL()));		
 		} else {
 			animeTitleLabel.setText("");
 			metaEpisodesLabel.setText("");
 			metaYearLabel.setText("");
 			metaStatusLabel.setText("");
 			metaRatingLabel.setText("");
-			metaCover.setImage(new Image("http://i.imgur.com/jmpe90k.jpg"));
+			metaCover.setImage(new Image("http://i.imgur.com/jmpe90k.jpg")); //filler image so that the imageview is never empty			
 		}
 	}
 }
